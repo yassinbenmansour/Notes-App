4f7a25146b78fb4300621a48c5f189f91c030cdf
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-04-17 23:56:18 +0000

    update

--- database/database.php
+++ database/database.php
@@ -1,31 +1,33 @@
 <?php
+
 class Database {
-    private $db ;
-  function __construct(){
+
+    private $db;
+
+    function __construct()
+    {
         $this->db = new PDO("mysql:host=localhost;dbname=Noteapp","root", "");
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-
-    public function add($data){
+    public function add($data)
+    {
         $name = $data['name'];
         $description = $data['description'];
 
         try {
-
-            $sql = "INSERT into Notes (name,description,created)
-                    VALUES (:name,:description,now())";
-
-                    $state = $this->db->prepare($sql);
-                    $state -> execute(array(":name"=>$name,
-                                            ":description"=>$description,
-        ));
-
-        }catch(PDOException $exp){
-            echo "Error" . $exp->getMessage();
+            $sql = "INSERT into Notes (name,description,created) VALUES (:name,:description,now())";
+            $state = $this->db->prepare($sql);
+            $state->execute(array(
+                ":name" => $name,
+                ":description" => $description
+            ));
+            
+            if($state){
+                echo "Note Ajoutée";
+            }
+        } catch (PDOException $exp) {
+            echo "error" . $exp->getMessage();
         }
     }
-
-
 }
-
--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 
 require('./database/database.php');
 $database = new Database();
+
+$data = array(
+    'name' => 'Yassine benmansour',
+    'description' => 'hello from yassine '
+);
+
+
+$database->add($data);
+
 ?>
