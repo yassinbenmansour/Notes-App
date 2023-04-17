8a5cccd202e9050ad8389c78a5609503c2535c7c
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-04-17 23:24:08 +0000

    update

---
+++ database/database.php
@@ -0,0 +1,31 @@
+<?php
+class Database {
+    private $db ;
+  function __construct(){
+        $this->db = new PDO("mysql:host=localhost;dbname=Noteapp","root", "");
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+
+    public function add($data){
+        $name = $data['name'];
+        $description = $data['description'];
+
+        try {
+
+            $sql = "INSERT into Notes (name,description,created)
+                    VALUES (:name,:description,now())";
+
+                    $state = $this->db->prepare($sql);
+                    $state -> execute(array(":name"=>$name,
+                                            ":description"=>$description,
+        ));
+
+        }catch(PDOException $exp){
+            echo "Error" . $exp->getMessage();
+        }
+    }
+
+
+}
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require('./database/database.php');
+$database = new Database();
+?>
