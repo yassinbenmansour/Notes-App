69349e8ecbfd71bcdcbd84620d46a6fd351e9f4b
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-04-18 00:13:33 +0000

    update

--- database/database.php
+++ database/database.php
@@ -1,16 +1,41 @@
 <?php
 
-class Database {
+class Database
+{
 
     private $db;
 
     function __construct()
     {
-        $this->db = new PDO("mysql:host=localhost;dbname=Noteapp","root", "");
+        $this->db = new PDO("mysql:host=localhost;dbname=Noteapp", "root", "");
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     public function add($data)
+    {
+        $name = $data['name'];
+        $description = $data['description'];
+
+        try {
+            $sql = "INSERT into Notes (name,description,created) VALUES (:name,:description,now())";
+
+
+            $state = $this->db->prepare($sql);
+            $state->execute(array(
+                ":name" => $name,
+                ":description" => $description,
+
+            ));
+
+            if ($state) {
+                echo "Note Ajoutée";
+            }
+        } catch (PDOException $exp) {
+            echo "error" . $exp->getMessage();
+        }
+    }
+
+    public function update($data)
     {
         $id = $data['id'];
         $name = $data['name'];
@@ -19,7 +44,6 @@ class Database {
         try {
             $sql = "UPDATE  Notes SET name=:name,description=:description WHERE id = :id";
 
-            // $sql = "INSERT into Notes (name,description,created) VALUES (:name,:description,now())";
             $state = $this->db->prepare($sql);
             $state->execute(array(
                 ":name" => $name,
@@ -28,10 +52,32 @@ class Database {
 
             ));
 
-            if($state){
-                 // echo "Note Ajoutée";
+            if ($state) {
                 echo "Note Modifiée";
+            }
+        } catch (PDOException $exp) {
+            echo "error" . $exp->getMessage();
+        }
+    }
 
+    public function delete($data)
+    {
+        $id = $data['id'];
+        
+
+        try {
+
+            $sql = "DELETE  FROM  Notes WHERE id = :id";
+
+            $state = $this->db->prepare($sql);
+            $state->execute(array(
+                ":id" => $id
+
+            ));
+
+            if ($state) {
+
+                echo "Note Supprimée";
             }
         } catch (PDOException $exp) {
             echo "error" . $exp->getMessage();
--- index.php
+++ index.php
@@ -4,9 +4,8 @@ require('./database/database.php');
 $database = new Database();
 
 $data = array(
-    'name' => 'Yassine benmansour',
-    'description' => 'hello from yassine update ',
-    'id' => '1'
+    'name' => 'sara benmansour',
+    'description' => 'hello from sara  ',
 );
 
 
